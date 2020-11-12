9d6a17ad7e4f246f61f42edc13e57cb1ee48571a
chu83 <59534807+chu83@users.noreply.github.com>
2020-11-12 16:01:06 +0900

    comprehension

---
+++ 05/while.py
@@ -0,0 +1,53 @@
+#while loop
+
+
+print('========== While loop 기본 ===========')
+count =1
+while count < 11:
+    print(count, end=' ')
+    count +=1
+else:
+    print('')
+
+
+print('========== 1~10 합 ===========')
+s, n = 0, 1
+while n<= 10:
+    s+=n
+    n+=1
+print(s)
+
+
+
+print('========== break ===========')
+i = 0
+while i < 10:
+    if i > 5:
+        break
+    print(i, end=' ')
+    i += 1
+else:
+    print('-----end loop')
+
+
+print('========== continue ===========')
+i = 0
+while i < 10:
+    if i <= 5:
+        continue
+    print(i, end=' ')
+    i +=1
+else:
+    print('---- end loop')
+
+
+
+print('========== 무한루프 ===========')
+i = 0
+while True:
+    print(i, end=' ')
+    if i >5:
+        break
+    i +=1
+
+    
