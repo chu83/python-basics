1aa420469e84a7e2542ccbf254800660826b842d
chu83 <59534807+chu83@users.noreply.github.com>
2020-11-13 15:34:56 +0900

    심볼테이블

---
+++ 06/test.py
@@ -0,0 +1,24 @@
+def f(i):
+    i = 20
+    return i
+
+def f2(seq):
+    seq[0] =10
+
+
+def func1(d):
+    print("hello world")
+
+def func2(name):
+    return "Helllo"
+
+
+a=10
+print(a)
+f(a)
+print(a)
+print(f(a))
+func1("Hi")
+b=func2("HI")
+print(b)
+print(func2("HI"))
